fa230a384f36a1521e3784d165f42969e51fd913
ITSOK\anonm <dev1ender@gmail.com>
2017-07-02 00:03:54 +0530

    final

---
+++ SevenTwo.java
@@ -0,0 +1,9 @@
+public class SevenTwo{
+	public static void main(String []args){
+	String Str = "This is a	Solution of hte assignment ";
+	String Check = "is";
+	if(Str.contains(Check)){
+	System.out.println("True: substring is present in the main string");
+	}
+	}
+}
