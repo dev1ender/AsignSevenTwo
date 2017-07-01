69ecb39147ab91c4b7853d0817290dc6cbd4de40
ITSOK\anonm <dev1ender@gmail.com>
2017-07-02 00:16:50 +0530

    last

--- SevenTwo.java
+++ SevenTwo.java
@@ -1,7 +1,10 @@
 public class SevenTwo{
 	public static void main(String []args){
+	//main string
 	String Str = "This is a	Solution of hte assignment ";
+	//sub string to be check
 	String Check = "is";
+	//contains method is used to check the sub string contained in main string or not
 	if(Str.contains(Check)){
 	System.out.println("True: substring is present in the main string");
 	}
